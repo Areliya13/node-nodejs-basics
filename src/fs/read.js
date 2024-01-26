f15595ec45409699cfd00708cae9675f8bba8170
Areliya13 <sharipovatamara@gmail.com>
2024-01-27 02:34:38 +0300

    feat: implemented FS read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(pathToFile, { encoding: "utf8" });
+    console.log(data);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
