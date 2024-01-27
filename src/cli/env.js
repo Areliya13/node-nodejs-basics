7f69f4f1c707e2a83818ac80376fa8bcfb222630
Areliya13 <sharipovatamara@gmail.com>
2024-01-28 02:15:29 +0300

    feat: implemented CLI env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const res = Object.entries(process.env)
+        .filter(([key, _]) => key.startsWith("RSS_"))
+        .map(([key, value]) => `${key}=${value}`)
+        .join("; ");
+    console.log(res);
 };
 
 parseEnv();
