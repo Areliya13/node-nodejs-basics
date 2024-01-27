fe1be9136f2432e4b341c22724bba3576a960690
Areliya13 <sharipovatamara@gmail.com>
2024-01-28 01:36:16 +0300

    feat: implemented FS delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await fs.unlink(pathToFile);
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await remove();
