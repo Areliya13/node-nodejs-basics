09fb8802413887823d5f3fe97fd8c725336fbcfb
Areliya13 <sharipovatamara@gmail.com>
2024-01-30 00:15:51 +0300

    feat: implemented child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import child_process from "node:child_process";
+import path from "node:path";
+import url from "node:url";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const child = child_process.fork(pathToFile, args);
+    child.send(args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 4, 9, "enough?"]);
