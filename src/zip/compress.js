c053f132beb4157261219dcea5658defbec968a0
Areliya13 <sharipovatamara@gmail.com>
2024-01-29 23:24:37 +0300

    feat: implemented zlib compress&decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+import zlib from "node:zlib";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "fileToCompress.txt");
+const pathToZip = path.join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const readableStream = fs.createReadStream(pathToFile);
+    const writableStream = fs.createWriteStream(pathToZip);
+
+    const gZip = zlib.createGzip();
+
+    readableStream.pipe(gZip).pipe(writableStream);
 };
 
 await compress();
