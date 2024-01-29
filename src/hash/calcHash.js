41b23cdf9490ecf7a705cd917148a0167a5df311
Areliya13 <sharipovatamara@gmail.com>
2024-01-29 22:37:43 +0300

    feat: implemented hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+import crypto from "node:crypto";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const data = await fs.readFile(pathToFile);
+    const dataHash = crypto.createHash("sha256").update(data).digest("hex");
+    console.log(dataHash);
 };
 
 await calculateHash();
