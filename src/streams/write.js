c20e6c4c60ec3b476f2eecb155b3f2604d310838
Areliya13 <sharipovatamara@gmail.com>
2024-01-29 22:48:11 +0300

    feat: implemented writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+
+const urlToPath = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(urlToPath);
+const pathToFile = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    const writableStream = fs.createWriteStream(pathToFile);
+    process.stdin.pipe(writableStream);
 };
 
 await write();
